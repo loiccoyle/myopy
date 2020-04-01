2c728dc04c2ffe12d01f32e8dfd57bccbace20c9
Loic Coyle <loic.coyle@hotmail.fr>
2020-04-01 13:43:01 +0200

    add comments

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,26 @@
-from pyconf.conf import PyConfig
+from pyconf import PyConfig
 
 def function():
     print('function print')
 
+class Parameters:
+    def __init__(self, parameters=[]):
+        self.parameters = parameters
+
+    def add_parameter(self, param: str):
+        self.parameters.append(param)
+
+test_dict = {'d': 5}
+
+params = Parameters(['a', 'b'])
 config = PyConfig('test_config.py')
-config.provide('test_function', function)
+# providing the objects
+config.provide('function', function)
+config.provide('params', params)
+config.provide('test_dict', test_dict)
+# running the file
 out = config.run()
-print(out)
+# our objects are modified !
+print('in test.py: ', out)
+print('in test.py: ', params.parameters)
+print('in test.py: ', test_dict)
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,2 +1,12 @@
-test_function()
+# This file is a mockup of a configuration file, the user does not need to know
+# where the objects come from or how to init them. As such, it has no imports,
+# and no objects are defined within it. They are provided at run time by the
+# PyConfig class.
 
+function()
+
+print('in test_config,py: ', params.parameters)
+params.add_parameter('c')
+
+print('in test_config,py: ', test_dict)
+test_dict['e'] = 10
